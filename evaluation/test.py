30d17e60b385f748400efa834593a907cf54dede
Jakub Powierza <jpowie01@gmail.com>
2018-12-21 00:33:32 +0100

    Fix script for evaluation

--- evaluation/test.py
+++ evaluation/test.py
@@ -10,30 +10,31 @@ import argparse
 import numpy as np
 
 from dataset import cifar10
+from cnn.resnet import resnet_v2
 from capsnet.capsulenet import CapsNet
 
 CAPSNET_ROUTINGS = 3  # TODO: Allow to override it!
 
 
-def load_model(network_type, weights):
+def evaluate(network_type, weights):
     if network_type == 'capsnet':
         model, eval_model, _ = CapsNet(input_shape=cifar10.IMAGE_SHAPE, n_class=cifar10.CLASSES, routings=CAPSNET_ROUTINGS)
-        model.summary()
-        model.load_weights(weights)
-        return model
     elif network_type == 'cnn':
-        print('Will be done later...')
+        model = resnet_v2(input_shape=cifar10.IMAGE_SHAPE, depth=12*9+2)
 
+    model.summary()
+    model.load_weights(weights)
 
-def evaluate(model):
-    if not model:
-        print('Model was not loaded...')
-        exit(0)
-    
     for rotation in [-45, -30, -15, 0, 15, 30, 45]:
-        x_test, y_test = cifar10.get_test_data_for_capsnet(rotation)
-        y_pred, x_reconstructed = model.predict(x_test, batch_size=100)
-        accuracy = np.sum(np.argmax(y_pred, 1) == np.argmax(y_test[0], 1))/y_test[0].shape[0]
+        if network_type == 'capsnet':
+            x_test, y_test = cifar10.get_test_data_for_capsnet(rotation)
+            y_pred, x_reconstructed = model.predict(x_test, batch_size=100)
+            accuracy = np.sum(np.argmax(y_pred, 1) == np.argmax(y_test[0], 1))/y_test[0].shape[0]
+        elif network_type == 'cnn':
+            x_test, y_test = cifar10.get_test_data_for_cnn(rotation)
+            scores = model.evaluate(x_test, y_test, verbose=1)
+            accuracy = scores[1]
+
         print('Rotation:', rotation, 'Accuracy:', accuracy)
 
 
@@ -43,6 +44,5 @@ if __name__ == '__main__':
     parser.add_argument('--weights', required=True, type=str)
     args = parser.parse_args()
 
-    model = load_model(args.network_type, args.weights)
-    evaluate(model)
+    evaluate(args.network_type, args.weights)
 
